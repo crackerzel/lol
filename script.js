6d2c5cc25f47185ecaae62937c380ef65dcebade
crackerzel <j2618770@trbvm.com>
2016-01-18 11:22:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+
+	document.body.innerHTML = "<img id='lol' src='https://pbs.twimg.com/profile_images/423147305605554176/EY-nBh68.jpeg'>";
+	document.getElementById('lol').style.width = "100%";
+	document.getElementById('lol').style.height = "100%";
+
+}
